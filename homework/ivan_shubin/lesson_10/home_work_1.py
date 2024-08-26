efbbd34d19ace13cb749329aeca0b037eba6fbb1
ivans <ivan.shubin.1989@mail.ru>
2024-08-26 13:38:23 +0300

    home_work_10

---
+++ homework/ivan_shubin/lesson_10/home_work_1.py
@@ -0,0 +1,13 @@
+#Задание №1: Декоратор, распечатывающий "finished"
+
+def finish_me(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        print("finished")
+    return wrapper
+
+@finish_me
+def example(text):
+    print(text)
+
+example('print me')
